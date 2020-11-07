92dd3a3ce10533882eb9e42945893ec7c863ff01
alexxck <boguslavets@gmail.com>
2020-11-07 18:32:06 +0200

    only ruby OK

--- lesson 3/alexey.boguslavets/project.sh
+++ lesson 3/alexey.boguslavets/project.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 docker network create alex_network
+
 docker run --rm -d -e POSTGRESS_PASSWORD=somepass -p 5432:5432 --net alex_network --name alex_postgres postgres:9.6.5
 docker run --rm -d -e REDIS_PASSWORD=somepass -p 6379:6379 --net alex_network --name alex_redis redis redis-server --requirepass somepass
-docker run -it --rm --net alex_network -p 9000:4567 -e DB=postgresql:somepass@alex_postgres:5432 -e CHACHE=redis://:somepass@alex_redis:6379/ -v "$PWD":/usr/src/app/ -w /usr/src/app ruby:2.6.0 ruby server.rb
+docker run -it --rm --net alex_network -p 9000:4567 -e DB=postgresql:somepass@alex_postgres:5432 -e CHACHE=redis://:somepass@alex_redis:6379/ -v /home/alex/devops-course-2020/lesson\ 3/:/usr/src/app/ -w /usr/src/app ruby:2.6.0 ruby server.rb
