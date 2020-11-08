aa6e12cd924fcaca8c7efe7db663ae8f9babb239
alexxck <boguslavets@gmail.com>
2020-11-08 14:15:27 +0200

    db cache error

--- lesson 3/alexey.boguslavets/project.sh
+++ lesson 3/alexey.boguslavets/project.sh
@@ -1,11 +1,16 @@
 #!/bin/bash
 
-docker network create alex_network
+NETWORK=alex_network
+PG_USER=pguser
+PG_PASSWORD=pgpass
+PG_HOST=pg
+PG_NAME=postgres
 
-DB="postgresql://postgresql:somepass@alex_postgress:5432"
-CACHE="redis://:somepass@0.0.0.0:6379/15"
+DB="postgresql://${PG_USER}:${PG_PASSWORD}@${PG_HOST}/${PG_NAME}"
+CACHE="redis://redis:6379/0"
 
-docker run --rm -d --net alex_network -p 9000:4567 --name alex_ruby -e DB=postgresql:somepass@alex_postgres:5432 -e CHACHE=redis://:somepass@alex_redis:6379/ \
--v /home/alex/devops-course-2020/lesson\ 3/:/usr/src/app/ -w /usr/src/app ruby:2.6.0 ruby server.rb
-docker run --rm -d --net alex_network -p 5432:5432 --name alex_postgres -e POSTGRESS_PASSWORD=somepass  postgres:9.6.5
-docker run --rm -d --net alex_network -p 6379:6379 --name alex_redis -e REDIS_PASSWORD=somepass  redis redis-server --requirepass somepass
+docker network create ${NETWORK}
+
+docker run --rm -d --net ${NETWORK} -p 5432:5432 --name alex_postgres -e PG_HOST=${PG_HOST} -e PG_USER=${PG_USER} -e PG_PASSWORD=${PG_PASSWORD} postgres:9.6.5-alpine
+docker run --rm -d --net ${NETWORK} -p 6379:6379 --name alex_redis redis:4-alpine
+docker run --rm --net ${NETWORK} -p 9000:4567 --name alex_ruby -e DB=${DB} -e CACHE=${CACHE} -v /home/alex/devops-course-2020/lesson\ 3/:/usr/src/app/ -w /usr/src/app ruby:2.6.0 ruby server.rb
